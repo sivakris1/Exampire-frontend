4a2472d6ffe94fccad3f8dcc0a20379c8f774431
sivakrish1 <sivapalaparthi003@gmail.com>
2026-01-30 19:50:41 +0530

    chore: setup frontend folder structure and page skeleton

---
+++ frontend/src/components/PaperCard.jsx
@@ -0,0 +1,11 @@
+const PaperCard = () => {
+  return (
+    <div>
+      <h3>Paper Title</h3>
+      <p>Exam | Year | Subject</p>
+      <button disabled>View</button>
+    </div>
+  );
+};
+
+export default PaperCard;
