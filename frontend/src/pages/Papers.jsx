d3407fbcb31d520c91e435c0cf55bde28f9f5ce3
sivakrish1 <sivapalaparthi003@gmail.com>
2026-02-01 19:50:46 +0530

    feat: fetch and render papers list from backend API

--- frontend/src/pages/Papers.jsx
+++ frontend/src/pages/Papers.jsx
@@ -1,20 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Papers = () => {
+
+  const [papers, setPapers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchPapers = async () => {
       try {
         const response = await axios.get("http://localhost:9000/api/papers/");
-        console.log(response.data);
+        setPapers(response.data.papers);
       } catch (error) {
         console.error("Error fetching papers:", error);
+      }
+      finally{
+        setLoading(false);
       }
     };
 
     fetchPapers();
   }, []);
 
-  return <div> Papers Page</div>;
+  return (
+     <div>
+      <h1>Question Papers</h1>
+
+      {papers.length === 0 ? (
+        <p>No papers found</p>
+      ) : (
+        papers.map((paper) => (
+          <div key={paper._id}>
+            <h3>{paper.examName}</h3>
+            <p>{paper.stream}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Papers;
