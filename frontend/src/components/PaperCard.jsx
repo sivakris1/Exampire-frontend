dbb35cf83c8bd0aebbdf512fb29d3bd9c7bc7e5d
sivakrish1 <sivapalaparthi003@gmail.com>
2026-02-01 20:44:15 +0530

    refactor: render papers list using reusable PaperCard component

--- frontend/src/components/PaperCard.jsx
+++ frontend/src/components/PaperCard.jsx
@@ -1,7 +1,14 @@
-const PaperCard = ({paper}) => {
+import { useNavigate } from "react-router-dom";
+
+const PaperCard = ({ paper }) => {
+  const navigate = useNavigate();
+  console.log(paper)
+
   return (
     <div>
-      <h4>{paper.paperTitle}</h4>
+      <h4 onClick={() => navigate(`/papers/${paper._id}`)}>{paper?.paperTitle || "Untitled Paper"}</h4>
+      <p>{paper.examName}</p>
+      <p>{paper.stream}</p>
     </div>
   );
 };
