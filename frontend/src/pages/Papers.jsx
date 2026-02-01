8f69c8542f401a52946c1a01fd17963e1371845f
sivakrish1 <sivapalaparthi003@gmail.com>
2026-02-01 20:33:09 +0530

    feat: filter papers by exam using query params

--- frontend/src/pages/Papers.jsx
+++ frontend/src/pages/Papers.jsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PaperCard from "../components/PaperCard";
+import { useSearchParams } from "react-router-dom";
 const Papers = () => {
 
   const [papers, setPapers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchParams] = useSearchParams();
+
+  const exam = searchParams.get('exam');
 
   useEffect(() => {
     const fetchPapers = async () => {
       try {
         const response = await axios.get("http://localhost:9000/api/papers/");
-        setPapers(response.data.papers);
-        console.log(response.data)
+        const allPapers = response.data.papers;
+
+        const filtered = exam
+          ? allPapers.filter((p) => p.examName === exam)
+          : allPapers;
+
+        setPapers(filtered);
+
       } catch (error) {
         console.error("Error fetching papers:", error);
       }
@@ -22,22 +32,17 @@ const Papers = () => {
     };
 
     fetchPapers();
-  }, []);
+  }, [exam]);
 
   return (
      <div>
-      <h1>Question Papers</h1>
+      <h2>{exam} Papers</h2>
 
       {papers.length === 0 ? (
         <p>No papers found</p>
       ) : (
         papers.map((paper) => (
-          <div key={paper._id}>
-            <h3>{paper.examName}</h3>
-            <p>{paper.stream}</p>
-
-            <PaperCard paper={paper} /> 
-          </div>
+          <PaperCard key={paper._id} paper={paper} />
         ))
       )}
     </div>
