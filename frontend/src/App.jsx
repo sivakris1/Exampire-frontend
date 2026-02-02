6c86f3f9232664a3c6fadf4578bc7718552ab669
sivakrish1 <sivapalaparthi003@gmail.com>
2026-02-02 22:56:31 +0530

    feat: add route for paper details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 import Papers from './pages/Papers'
 import Exams from './pages/Exams'
+import PaperDetails from './pages/PaperDetails'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Exams/>} />
       <Route path="/papers" element={<Papers />} />
+      <Route path="/papers/:id" element={<PaperDetails/>} />
     </Routes>
   )
 }
