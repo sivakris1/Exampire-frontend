8e1b9ff936b5969abf86ef21442c462dc8a6845c
sivakrish1 <sivapalaparthi003@gmail.com>
2026-02-10 22:34:58 +0530

    fix: correctly derive exam from route params

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:9000/api";
 
-export const getPapers = () => {
-  return axios.get(`${API_BASE_URL}/papers`);
+export const getPapers = (params) => {
+  return axios.get(`${API_BASE_URL}/papers`,{params});
 };
 
 export const getPaperById = (id) => {
