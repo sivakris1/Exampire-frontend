b7e832012de97643b352a48b3f555a9d7bba0687
sivakrish1 <sivapalaparthi003@gmail.com>
2026-02-27 21:03:03 +0530

    modify: update routes and add route for navbar and home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import {Route,Routes} from 'react-router-dom'
-import Papers from './pages/Papers'
-import Exams from './pages/Exams'
-import PaperDetails from './pages/PaperDetails'
+import { Route, Routes } from "react-router-dom";
+import Papers from "./pages/Papers";
+import Exams from "./pages/Exams";
+import PaperDetails from "./pages/PaperDetails";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Exams/>} />
-      <Route path="/papers" element={<Papers />} />
-      <Route path="/papers/:id" element={<PaperDetails/>} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/papers" element={<Papers />} />
+        <Route path="/papers/:id" element={<PaperDetails />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
