50ae0ec6b90d27972840ff014a146e942b57e4c0
sivakrish1 <sivapalaparthi003@gmail.com>
2026-03-11 22:24:48 +0530

    feat: add Logout button functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const handleLogout = () => {
+  localStorage.removeItem("token");
+  window.location.reload();
+};
+
   return (
     <nav style={{ padding: "10px", background: "#eee" }}>
       <Link to="/">Home</Link>
+      <button onClick={handleLogout}>Logout</button>
     </nav>
   );
 };
