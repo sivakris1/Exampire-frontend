75bd7193f8b7e3283463de649ff00459b5e0c5e4
sivakrish1 <sivapalaparthi003@gmail.com>
2026-03-20 16:21:27 +0530

    feat: add trending papers section on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getTrendingPapers } from '../api/client';
 
 const Home = () => {
+  const [loading,setLoading] = useState(false);
+  const [trending,setTrending] = useState([])
+
+  useEffect(()=>{
+    const fetchTrending = async () => {
+    try {
+      const res = await getTrendingPapers();
+      setTrending(res.data.papers);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchTrending();
+  },[])
   return (
+    <>
     <div>
       Home Page
     </div>
+
+    <h2>Trending papers</h2>
+
+    {loading ? (<p>Loading...</p>) 
+    : 
+    trending.length === 0 ? (<p>No trending Papers found</p>
+
+    ): (
+      trending.map((paper)=>(
+        <div key={paper._id}>
+          <h4>{paper.paperTitle}</h4>
+      <p>{paper.examName} - {paper.year}</p>
+        </div>
+      ))
+    )
+    }
+
+    </>
   )
 }
 
